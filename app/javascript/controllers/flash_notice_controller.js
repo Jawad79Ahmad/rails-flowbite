3ac498618b6e7a55ae2361b7ae15056db9929644
Jawad Ahmad <Jawad79Ahmad>
2023-12-29 15:46:25 +0500

    alert customized

--- app/javascript/controllers/flash_notice_controller.js
+++ app/javascript/controllers/flash_notice_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     setTimeout(() => {
-      $(this.element).remove();
+      $(this.element).find('button').click();
     }, 2000);
   }
 }
